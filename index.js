582f29b2a2b61032d03c866e7df53c0d23214a03
Lesley <14810964+looshlee@users.noreply.github.com>
2021-01-30 15:56:02 -0800

    Update index.js

--- index.js
+++ index.js
@@ -2,13 +2,5 @@ const core = require('@actions/core');
 const github = require('@actions/github');
 const fetch = require("node-fetch");
 
-var Pokedex = require('pokedex-promise-v2');
-var P = new Pokedex();
-
-  P.getPokemonByName('eevee') // with Promise
-    .then(function(response) {
-      console.log(response);
-    })
-    .catch(function(error) {
-      console.log('There was an ERROR: ', error);
-    });
+const client = core.getInput('client');
+const token = core.getInput('token');
