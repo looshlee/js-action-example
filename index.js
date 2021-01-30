adcf6f28cbb8500a9a020c06421fd6f0729c0972
Lesley <14810964+looshlee@users.noreply.github.com>
2021-01-30 15:26:55 -0800

    Update index.js

--- index.js
+++ index.js
@@ -12,16 +12,3 @@ var P = new Pokedex();
     .catch(function(error) {
       console.log('There was an ERROR: ', error);
     });
-
-  P.getPokemonByName(34, function(response, error) { // with callback
-      if(!error) {
-        console.log(response);
-      } else {
-        console.log(error)
-      }
-    });
-
-  P.resource(['/api/v2/pokemon/36', 'api/v2/berry/8', 'https://pokeapi.co/api/v2/ability/9/'])
-    .then(function(response) {
-      console.log(response); // resource function accepts singles or arrays of URLs/paths
-    });
