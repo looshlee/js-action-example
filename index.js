0353586acc174612a27af99ab05c277c9ed3e59b
Lesley <14810964+looshlee@users.noreply.github.com>
2021-01-31 11:48:32 -0800

    Update index.js

--- index.js
+++ index.js
@@ -4,12 +4,30 @@ const { Octokit } = require("@octokit/rest");
 
 // const token = core.getInput('github_token');
 
+// Create instance of Octokit
 const octokit = new Octokit();
-let userData
+ 
+// get my username because I need my GITHUB_ID to do subsequent queries
+
+async function getGitHubID () {
+    octokit.users.getByUsername({
+    looshlee,
+  })
+}
+
+const looshleeID = await getGitHubID();
+
+// I should set my GITHUB_ID to a variable, but the above call is a promise! 
+// Good read: https://codeburst.io/a-simple-guide-to-es6-promises-d71bacd2e13a
+
+console.log("looshleeID", looshleeID)
 
 // https://octokit.github.io/rest.js/v18#users-list
 octokit.users
-  .list()
+  .list({
+    per_page: 100
+    since: looshleeID
+   })
   .then(({ data }) => {
     console.log(data, "userData")
     console.log(data[0], "first person")
