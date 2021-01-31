8bab867b75b35589e8bcb49a66139f6a8d185510
Lesley <14810964+looshlee@users.noreply.github.com>
2021-01-31 11:11:32 -0800

    Update index.js

--- index.js
+++ index.js
@@ -6,15 +6,21 @@ const { Octokit } = require("@octokit/rest");
 
 const octokit = new Octokit();
 
-// Compare: https://docs.github.com/en/rest/reference/repos/#list-organization-repositories
-octokit.repos
-  .listForOrg({
-    org: "octokit",
-    type: "public",
-  })
+octokit.users
+  .list()
   .then(({ data }) => {
     console.log("data", data)
   });
+
+// Compare: https://docs.github.com/en/rest/reference/repos/#list-organization-repositories
+// octokit.repos
+//   .listForOrg({
+//     org: "octokit",
+//     type: "public",
+//   })
+//   .then(({ data }) => {
+//     console.log("data", data)
+//   });
 
 // octokit.issues.listForRepo({
 //   owner,
