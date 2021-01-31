8cae7cb91919fbfccdf29000baf8dd3d0ec61310
Lesley <14810964+looshlee@users.noreply.github.com>
2021-01-31 13:02:02 -0800

    Update index.js

--- index.js
+++ index.js
@@ -39,13 +39,13 @@ function userNameOnly(data) {
   //  For each user, map data.login to replace user
 
  
- let oldResults = data.map(function(user) {
-    return user => data.login
-})
+//  let oldResults = data.map(function(user) {
+//     return user => data.login
+// })
  
  let results = data.map(user => data.login)
  
- console.log(oldResults, "oldResults")
+//  console.log(oldResults, "oldResults")
  console.log(results, "results")
 }
 
