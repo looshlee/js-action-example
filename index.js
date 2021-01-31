c94318a241ab4130cbeb58d87e1598766c283687
Lesley <14810964+looshlee@users.noreply.github.com>
2021-01-30 16:47:45 -0800

    Update index.js

--- index.js
+++ index.js
@@ -2,5 +2,20 @@ const core = require('@actions/core');
 const github = require('@actions/github');
 const fetch = require("node-fetch");
 
-const client = core.getInput('client');
-const token = core.getInput('token');
+const twitter_token = core.getInput('twitter_token');
+
+
+var myHeaders = new Headers();
+myHeaders.append("Authorization", "Bearer " + twitter_token);
+myHeaders.append("Cookie", "guest_id=v1%3A161205311113227072; personalization_id=\"v1_mNAAzbye8dMSjSr3YnmuVg==\"");
+
+var requestOptions = {
+  method: 'GET',
+  headers: myHeaders,
+  redirect: 'follow'
+};
+
+fetch("https://api.twitter.com/2/users/by/username/chrissyteigen", requestOptions)
+  .then(response => response.text())
+  .then(result => console.log(result))
+  .catch(error => console.log('error', error));
