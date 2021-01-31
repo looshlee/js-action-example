5cb60dbe133d0f4335f937fdc94555082bb7af76
Lesley <14810964+looshlee@users.noreply.github.com>
2021-01-31 11:14:01 -0800

    Update index.js

--- index.js
+++ index.js
@@ -2,16 +2,22 @@ const core = require('@actions/core');
 const github = require('@actions/github');
 const { Octokit } = require("@octokit/rest");
 
-// const twitter_token = core.getInput('twitter_token');
+// const token = core.getInput('github_token');
 
 const octokit = new Octokit();
 
+let userData = []
+
+// https://octokit.github.io/rest.js/v18#users-list
 octokit.users
   .list()
   .then(({ data }) => {
-    console.log("data", data)
+    let userData = data
   });
 
+console.log(userData, "userData")
+console.log(userData[0], "first person")
+console.log(userData.length, "length")
 // Compare: https://docs.github.com/en/rest/reference/repos/#list-organization-repositories
 // octokit.repos
 //   .listForOrg({
